6e9ab61b82580e6bf2b782eaf2e068910587e3a5
岩崎潤 <iwasaki.jun90@gmail.com>
2021-06-03 16:21:49 +0900

    バリテーション機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :last_name, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 end
