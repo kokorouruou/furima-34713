6a7d39c907644d9893cf27a6ab56b3a46dcf22e3
岩崎潤 <iwasaki.jun90@gmail.com>
2021-06-04 23:49:34 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {'test'}
-    email                   {'test@test'}
-    password                {'test0000'}
-    password_confirmation   {password}
-    last_name               {'田中'}
-    first_name              {'太郎'}
-    last_name_kana          {'タナカ'}
-    first_name_kana         {'タロウ'}
-    birthday                {'1980/01/01'}
+    nickname                { 'test' }
+    email                   { 'test@test' }
+    password                { 'test0000' }
+    password_confirmation   { password }
+    last_name               { '田中' }
+    first_name              { '太郎' }
+    last_name_kana          { 'タナカ' }
+    first_name_kana         { 'タロウ' }
+    birthday                { '1980/01/01' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
+  describe 'ユーザー新規登録' do
     before do
       @user = FactoryBot.build(:user)
     end
-    it "メールアドレスが必須であること" do
+    it 'メールアドレスが必須であること' do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
-    it "メールアドレスが一意性であること" do
+    it 'メールアドレスが一意性であること' do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include "Email has already been taken"
+      expect(another_user.errors.full_messages).to include 'Email has already been taken'
     end
-    it "メールアドレスは、@を含む必要があること" do
+    it 'メールアドレスは、@を含む必要があること' do
       @user.email = 'testtest'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Email is invalid"
+      expect(@user.errors.full_messages).to include 'Email is invalid'
     end
-    it "パスワードが必須であること" do
-      @user.password = ""
+    it 'パスワードが必須であること' do
+      @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
-    it "パスワードは、6文字以上での入力が必須であること" do
-      @user.password = "test1"
+    it 'パスワードは、6文字以上での入力が必須であること' do
+      @user.password = 'test1'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
     end
-    it "パスワードとパスワード（確認用）、値の一致が必須であること" do
-      @user.password_confirmation = "hoge1234"
+    it 'パスワードとパスワード（確認用）、値の一致が必須であること' do
+      @user.password_confirmation = 'hoge1234'
       @user.valid?
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
