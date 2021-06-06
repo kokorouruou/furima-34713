738d4ca64c4ef1da2e000b39f6578477d78fa2d4
岩崎潤 <iwasaki.jun90@gmail.com>
2021-06-06 15:58:02 +0900

    レビュー後修正2回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :password, length: { minimum: 6 },format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
-with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-  validates :last_name
-  validates :first_name
+with_options presence: true do
+    validates :nickname
+    validates :password, length: { minimum: 6 },format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday
 end
-with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-  validates :last_name_kana
-  validates :first_name_kana
-end
-  validates :birthday, presence: true
 
   #has_many :items
   #has_many :comments
