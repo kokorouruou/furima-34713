84e4bf2f916cfa851a2be6aeccb6b330010606e8
岩崎潤 <iwasaki.jun90@gmail.com>
2021-06-11 21:48:16 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :day_to_ship
 
-  has_one_attached : image
+  has_one_attached :image
   
-  with_options presence: true do
-    validates :title
-    validates :text
-    validates :price
-    validates :category_id
-    validates :product_condition_id
-    validates :delivery_charge_id
-    validates :day_to_ship_id
-  end
+  validates :title, :text, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :product_condition_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :day_to_ship_id, numericality: { other_than: 1 }
 
 end
