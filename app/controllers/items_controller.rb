46fd0ec5258bbd1a9d2ac04477d78e3676354d4d
岩崎潤 <iwasaki.jun90@gmail.com>
2021-06-19 19:52:35 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end 
+
   private
 
   def item_params
     params.require(:item).permit(:title, :text, :price, :image, :category_id, :product_condition_id, :delivery_charge_id,
                                  :prefecture_id, :day_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+      unless user_signed_in? && current_user.id == @item.user_id
+        redirect_to action: :index
+    end
   end
 end
